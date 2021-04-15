a69cb8a4426b0faf68e13a6358d8ea4acdbc9cdc
John Kim <kakwak123@gmail.com>
2021-04-15 17:43:02 +1000

    added library stuff

---
+++ mylibraryapp/routes/authorRouter.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const authorRouter = express.Router()
+
+const authorController = require('../controllers/authorController.js')
+
+authorRouter.get('/', (req,res) => authorController.getAllAuthors(req, res))
+authorRouter.get('/:id', (req, res) => authorController.getAuthorByID(req, res))
+authorRouter.post('/', (req, res) => authorController.postAuthorDetail(req, res))
+
+module.exports = authorRouter
